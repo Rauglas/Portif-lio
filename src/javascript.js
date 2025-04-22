00e93fc89934ad1acf2a322f5723defed1200095
Saulo Camilo <saulo.camilo@yahoo.com.br>
2025-04-22 15:15:34 -0300

    Add files via upload

---
+++ src/javascript.js
@@ -0,0 +1,6 @@
+// Aqui você pode adicionar interatividade, por exemplo, um efeito de animação quando rolar a página
+
+// Função para mostrar mensagem quando clicar no projeto
+document.querySelector('.projeto a').addEventListener('click', function(event) {
+    alert('Você está sendo redirecionado para o projeto!');
+});
